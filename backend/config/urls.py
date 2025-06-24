de2313fd84169f58fdc2b369760ad7dcf7ac2787
myo <marie.nicolas29@hotmail.com>
2025-06-24 20:45:03 +0200

    fix(api): add default root view and update allowed hosts for local access

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("API Mollidays backend OK")
 
 urlpatterns = [
+    path('', home),  # Ajoute cette ligne
     path('admin/', admin.site.urls),
 ]
